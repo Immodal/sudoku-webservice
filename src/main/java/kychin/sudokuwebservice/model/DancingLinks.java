7d3fc6ee885f1928f68114322ffb625ab3919017
Immodal <kychin.n@gmail.com>
2020-07-09 15:52:52 +1000

    Implemented DancingLinks.java

---
+++ src/main/java/kychin/sudokuwebservice/model/DancingLinks.java
@@ -0,0 +1,175 @@
+package kychin.sudokuwebservice.model;
+
+import java.util.*;
+
+public class DancingLinks {
+    /**
+     * Converts an Exact Cover Matrix from a boolean[][] to a Dancing Links representation.
+     * @param ecm Exact Cover Matrix
+     * @return Dancing Links Exact Cover Matrix
+     */
+    public static Column fromExactCover(boolean[][] ecm) {
+        List<Column> columns = new ArrayList<>();
+        for (int i=0; i<ecm[0].length; i++) {
+            columns.add(new Column(i));
+            // Insert new columns to the right of their predecessors
+            if (i>0) columns.get(i-1).insertRight(columns.get(i));
+        }
+
+        // For each row in the ecMatrix,
+        for (int i=0; i<ecm.length; i++) {
+            Node prevNode = null;
+            // And each column in the ecMatrix,
+            for (int j=0; j<ecm[0].length; j++) {
+                // If row i satisfies column j, create a node here
+                if (ecm[i][j]) {
+                    Column c = columns.get(j);
+                    Node node = new Node(i, c);
+                    // Add node to column and insert it to the right of prevNode
+                    c.add(node, prevNode);
+                    prevNode = node;
+                }
+            }
+        }
+
+        // Insert root object
+        Column root = new Column(-1);
+        columns.get(columns.size() - 1).insertRight(root);
+        return root;
+    }
+
+    /**
+     * Base Class for Objects within the Dancing Links Matrix
+     */
+    protected static class Node {
+        protected Node up = this;
+        protected Node down = this;
+        protected Node left = this;
+        protected Node right = this;
+
+        private int id;
+        private Column column;
+
+        public Node(int id) {
+            this.id = id;
+        }
+
+        public Node(int id, Column column) {
+            this(id);
+            this.column = column;
+        }
+
+        /**
+         * Inserts the Node n to the right of this Node.
+         * @param n Node to be inserted
+         */
+        public void insertRight(Node n) {
+            // Update pointers in n
+            n.left = this;
+            n.right = this.right;
+            // Update pointers in new n.right, originally points to this, now n
+            n.right.left = n;
+            // Update pointers in this
+            this.right = n;
+        }
+
+        /**
+         * Inserts the Node n below this Node.
+         * @param n Node to be inserted
+         */
+        public void insertDown(Node n) {
+            // Update pointers in n
+            n.up = this;
+            n.down = this.down;
+            // Update pointers in new n.down, originally points to this, now n
+            n.down.up = n;
+            // Update pointers in this
+            this.down = n;
+        }
+    }
+
+    /**
+     * Column Node in the Dancing Links Matrix.
+     */
+    protected static class Column extends Node {
+        private int size = 0;
+
+        public int getSize() {
+            return size;
+        }
+
+        public Column(int id) {
+            super(id);
+        }
+
+        /**
+         * Adds Node n to this Column and inserts it to the right of Node left.
+         * Only use in construction.
+         * @param n Node to be added
+         * @param left Node to insert n to the right of
+         */
+        protected void add(Node n, Node left) {
+            // Because iteration is going top to bottom, insertion will always
+            // be at the bottom of the column (which is always above the header)
+            // Vertical Link
+            this.up.insertDown(n);
+            this.size++;
+            // Because iteration is going left to right, insertion will always
+            // to the right of the previous node
+            // Horizontal Link
+            if (left!=null) left.insertRight(n);
+        }
+
+        /**
+         * Covers this column to remove it from the Dancing Links Matrix.
+         */
+        public void cover() {
+            // Point column header's left right neighbors at each other
+            this.right.left = this.left;
+            this.left.right = this.right;
+            // Going top to bottom, i being a row in the column
+            Node i = this.down;
+            while (i != this) {
+                // Left to right, j a node in row i satisfies
+                Node j = i.right;
+                while (j != i) {
+                    // Point j's up down neighbors at each other
+                    j.down.up = j.up;
+                    j.up.down = j.down;
+                    // Because j has been covered, update the size of the column it belongs to
+                    j.column.size--;
+                    // Continue to the right
+                    j = j.right;
+                }
+                // Continue to the bottom
+                i = i.down;
+            }
+        }
+
+        /**
+         * Uncovers this column to reintroduce it to the Dancing Links Matrix. Works in reverse of cover().
+         */
+        public void uncover() {
+            // Going bottom to top, i being a row in the column
+            Node i = this.up;
+            while (i != this) {
+                // Right to left, j being columns that row i satisfies
+                Node j = i.left;
+                while (j != i) {
+                    // Because j has been uncovered, update the size of the column it belongs to
+                    j.column.size++;
+                    // Point j's up down neighbors to j again
+                    j.down.up = j;
+                    j.up.down = j;
+                    // Continue to the left
+                    j = j.left;
+                }
+                // Continue to the top
+                i = i.up;
+            }
+            // Point column header's left right neighbors at column again
+            this.right.left = this;
+            this.left.right = this;
+        }
+    }
+}
---
+++ src/test/java/kychin/sudokuwebservice/model/DancingLinksTests.java
@@ -0,0 +1,124 @@
+package kychin.sudokuwebservice.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DancingLinksTests {
+    @Test
+    void nodesInitializeWithCircularReferences() {
+        DancingLinks.Node node = new DancingLinks.Node(0);
+        assertEquals(node.left, node);
+        assertEquals(node.right, node);
+        assertEquals(node.up, node);
+        assertEquals(node.down, node);
+    }
+
+    @Test
+    void nodesInsertRight() {
+        DancingLinks.Node node1 = new DancingLinks.Node(0);
+        DancingLinks.Node node2 = new DancingLinks.Node(1);
+        DancingLinks.Node node3 = new DancingLinks.Node(2);
+
+        node1.insertRight(node2);
+        assertEquals(node1.left, node2);
+        assertEquals(node1.right, node2);
+        assertEquals(node2.left, node1);
+        assertEquals(node2.right, node1);
+        node2.insertRight(node3);
+        assertEquals(node2.left, node1);
+        assertEquals(node2.right, node3);
+        assertEquals(node3.left, node2);
+        assertEquals(node3.right, node1);
+        assertEquals(node1.left, node3);
+        assertEquals(node1.right, node2);
+    }
+
+    @Test
+    void nodesInsertDown() {
+        DancingLinks.Node node1 = new DancingLinks.Node(0);
+        DancingLinks.Node node2 = new DancingLinks.Node(1);
+        DancingLinks.Node node3 = new DancingLinks.Node(2);
+
+        node1.insertDown(node2);
+        assertEquals(node1.up, node2);
+        assertEquals(node1.down, node2);
+        assertEquals(node2.up, node1);
+        assertEquals(node2.down, node1);
+        node2.insertDown(node3);
+        assertEquals(node2.up, node1);
+        assertEquals(node2.down, node3);
+        assertEquals(node3.up, node2);
+        assertEquals(node3.down, node1);
+        assertEquals(node1.up, node3);
+        assertEquals(node1.down, node2);
+    }
+
+    @Test
+    void importsECMatrix() {
+        boolean[][] ecm = {
+                {true, false, false, true, true},
+                {true, false, true, false, true},
+                {false, true, false, false, false},
+                {false, true, false, false, true},
+        };
+
+        DancingLinks.Column root = DancingLinks.fromExactCover(ecm);
+        assertEquals(root, root.right.right.right.right.right.right);
+        assertEquals(root, root.left.left.left.left.left.left);
+        checkColumn(root.right, 2);
+        checkColumn(root.right.right, 2);
+        checkColumn(root.right.right.right, 1);
+        checkColumn(root.right.right.right.right, 1);
+        checkColumn(root.right.right.right.right.right, 3);
+
+        assertEquals(root.right.down.right.down, root.left.left);
+        assertEquals(root.right.right.down, root.right.right.down.left);
+        assertEquals(root.left.up.left, root.right.right.down.down);
+        assertNotSame(root.right.down, root.right.down.left);
+        assertNotSame(root.right.down, root.right.down.right);
+    }
+
+    /*
+    Checks if the column was created correctly
+     */
+    private void checkColumn(DancingLinks.Node col, int size) {
+        assertEquals(((DancingLinks.Column) col).getSize(), size);
+        DancingLinks.Node current = col;
+        for (int i=0; i<size+1; i++) {
+            current = current.down;
+            if(i<size) assertNotSame(col, current);
+            else assertEquals(col, current);
+        }
+    }
+
+    @Test
+    void columnsCoverAndUncover() {
+        boolean[][] ecm = {
+                {true, false, false, true, true},
+                {true, false, true, false, true},
+                {false, true, false, false, false},
+                {false, true, false, false, true},
+        };
+
+        DancingLinks.Column root = DancingLinks.fromExactCover(ecm);
+        DancingLinks.Column c2 = (DancingLinks.Column) root.right.right.right;
+        c2.cover();
+        assertEquals(root.right, root.right.right.right.right.right.right); // Column gone
+        assertEquals(root.right, root.right.down.down); // row gone
+        assertEquals(((DancingLinks.Column)root.right).getSize(), 1);
+        c2.uncover();
+        assertEquals(root.right, root.right.right.right.right.right.right.right); // Column back
+        assertEquals(root.right.down.down.left, root.right.down.down.right.right); // row back
+        assertEquals(((DancingLinks.Column)root.right).getSize(), 2);
+
+        DancingLinks.Column c1 = (DancingLinks.Column) root.right.right;
+        c1.cover();
+        assertEquals(root.right, root.right.right.right.right.right.right); // Column gone
+        assertEquals(root.left.up, root.right.down.right.right.down); // rows gone
+        assertEquals(((DancingLinks.Column)root.left).getSize(), 2);
+        c1.uncover();
+        assertEquals(root.right, root.right.right.right.right.right.right.right); // Column back
+        assertEquals(root.left.up, root.right.down.right.right.down.down); // rows back
+        assertEquals(((DancingLinks.Column)root.left).getSize(), 3);
+    }
+}
